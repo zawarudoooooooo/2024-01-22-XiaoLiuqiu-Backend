8ad759ff0193ff59a1e1dbb3da7214978e331826
Jerrylac <j78779915@gmail.com>
2024-01-04 15:06:19 +0800

    ADD Java

---
+++ src/main/java/com/example/XiaoLiuqiu/XiaoLiuqiuApplication.java
@@ -0,0 +1,13 @@
+package com.example.XiaoLiuqiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaoLiuqiuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XiaoLiuqiuApplication.class, args);
+	}
+
+}
