884c0047b805a963b7c59fc2d3be631d848d1c9e
Jerrylac <j78779915@gmail.com>
2024-01-08 09:57:26 +0800

    RtnCode ADD and member DAO edit and member service ADD

---
+++ src/main/java/com/example/XiaoLiuqiu/constants/RtnCode.java
@@ -0,0 +1,35 @@
+package com.example.XiaoLiuqiu.constants;
+
+public enum RtnCode {
+
+	SUCCESSFUL(200,"Successful!!"),//
+	;
+	
+	private int code;
+	
+	private String message;
+
+	private RtnCode(int code, String message) {
+		this.code = code;
+		this.message = message;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+	
+}
--- src/main/java/com/example/XiaoLiuqiu/repository/MemberDAO.java
+++ src/main/java/com/example/XiaoLiuqiu/repository/MemberDAO.java
@@ -1,5 +1,12 @@
 package com.example.XiaoLiuqiu.repository;
 
-public interface MemberDAO {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.XiaoLiuqiu.entity.Member;
+
+
+@Repository
+public interface MemberDAO extends JpaRepository<Member, String> {
 
 }
