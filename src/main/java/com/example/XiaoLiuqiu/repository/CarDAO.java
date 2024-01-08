fdd0211d5439bb6e863a9c117b32c540474c5a90
spinCarrot <h27732197@gmail.com>
2024-01-08 20:17:08 +0800

    car search add

--- src/main/java/com/example/XiaoLiuqiu/repository/CarDAO.java
+++ src/main/java/com/example/XiaoLiuqiu/repository/CarDAO.java
@@ -1,5 +1,17 @@
 package com.example.XiaoLiuqiu.repository;
 
-public interface CarDAO {
+import org.springframework.stereotype.Repository;
 
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.XiaoLiuqiu.entity.Car;
+
+
+
+@Repository
+public interface CarDAO extends JpaRepository<Car, String>{
+	public List<Car>findByCarIdContainingAndCarTypeIdAndCarIntroduceContaining(String carId, int carTypeId, String carIntroduce);
+	public List<Car>findByCarIdContainingAndCarIntroduceContaining(String carId,  String carIntroduce);
 }
