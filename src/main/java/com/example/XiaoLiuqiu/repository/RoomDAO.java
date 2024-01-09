2e829e3b1cacfa11918822fb1060ed978683ce09
Roy <wave7410@gmail.com>
2024-01-09 09:31:34 +0800

    search update

--- src/main/java/com/example/XiaoLiuqiu/repository/RoomDAO.java
+++ src/main/java/com/example/XiaoLiuqiu/repository/RoomDAO.java
@@ -1,11 +1,18 @@
 package com.example.XiaoLiuqiu.repository;
 
+
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.XiaoLiuqiu.entity.Room;
+import com.example.XiaoLiuqiu.vo.RoomGetRes;
 
 @Repository
 public interface RoomDAO extends JpaRepository<Room, String>{
 
+	public List<Room> findByRoomIdContaining(String roomId);
+	
 }
--- src/main/java/com/example/XiaoLiuqiu/service/impl/RoomServiceImpl.java
+++ src/main/java/com/example/XiaoLiuqiu/service/impl/RoomServiceImpl.java
@@ -1,10 +1,16 @@
 package com.example.XiaoLiuqiu.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
+import com.example.XiaoLiuqiu.constants.RtnCode;
+import com.example.XiaoLiuqiu.entity.Room;
 import com.example.XiaoLiuqiu.repository.RoomDAO;
 import com.example.XiaoLiuqiu.service.ifs.RoomService;
+import com.example.XiaoLiuqiu.vo.RoomGetRes;
 import com.example.XiaoLiuqiu.vo.RoomRes;
 
 @Service
@@ -14,11 +20,19 @@ public class RoomServiceImpl implements RoomService{
 	private RoomDAO roomDao;
 
 	@Override
-	public RoomRes roomInfo(String roomId, int roomTypeId, String roomIntroduce) {
-		
-		return null;
+	public RoomRes search(String roomId) {
+		roomId = !StringUtils.hasText(roomId) ? "" : roomId;
+//		roomTypeId = roomTypeId <= 10 ? 0 : roomTypeId;
+		List<Room> res = roomDao.findByRoomIdContaining(roomId);
+		return new RoomGetRes(RtnCode.SUCCESSFUL.getCode(), RtnCode.SUCCESSFUL.getMessage(), res);
 	}
-	
-	
+
+
+
+
+
+
+
+
 
 }
